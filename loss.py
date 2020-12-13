3e3857348fb30acb13131e5ea702b9ad20ac81ee
kpriyanshu256 <kpriyanshu256@gmail.com>
2020-12-13 13:10:36 +0530

    Label Smoothening added

--- loss.py
+++ loss.py
@@ -36,3 +36,37 @@ class FocalLoss(nn.Module):
             return torch.mean(F_loss)
         else:
             return F_loss
+
+
+class LabelSmoothingLoss(torch.nn.Module):
+    def __init__(self, smoothing: float = 0.1, reduction="mean", weight=None):
+        super(LabelSmoothingLoss, self).__init__()
+        self.epsilon = smoothing
+        self.reduction = reduction
+        self.weight = weight
+
+    def reduce_loss(self, loss):
+        if self.reduction == "mean":
+            return loss.mean()
+        elif self.reduction == "sum":
+            return loss.sum()
+        else:
+            return loss
+
+    def linear_combination(self, x, y):
+        return self.epsilon * x + (1 - self.epsilon) * y
+
+    def forward(self, preds, target):
+        if self.weight is not None:
+            self.weight = self.weight.to(preds.device)
+
+        if self.training:
+            n = preds.size(-1)
+            log_preds = F.log_softmax(preds, dim=-1)
+            loss = self.reduce_loss(-log_preds.sum(dim=-1))
+            nll = F.nll_loss(
+                log_preds, target, reduction=self.reduction, weight=self.weight
+            )
+            return self.linear_combination(loss / n, nll)
+        else:
+            return torch.nn.functional.cross_entropy(preds, target, weight=self.weight)
