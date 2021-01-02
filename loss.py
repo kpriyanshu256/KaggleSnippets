8d5736d5a440b5c5d937a65c3077681ce48d9a96
kpriyanshu256 <kpriyanshu256@gmail.com>
2021-01-02 21:41:21 +0530

    Symmetric CE loss

--- loss.py
+++ loss.py
@@ -101,3 +101,26 @@ class FocalCosineLoss(nn.Module):
             focal_loss = torch.mean(focal_loss)
 
         return cosine_loss + self.xent * focal_loss
+
+
+#################### Symmetric Cross Entropy Loss ##########################
+# https://arxiv.org/abs/1908.06112
+
+
+class SymmetricCrossEntropy(nn.Module):
+
+    def __init__(self, alpha=0.1, beta=1.0, num_classes= 5):
+        super(SymmetricCrossEntropy, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.num_classes = num_classes
+
+    def forward(self, logits, targets, reduction='mean'):
+        onehot_targets = torch.eye(self.num_classes)[targets].cuda()
+        ce_loss = F.cross_entropy(logits, targets, reduction=reduction)
+        rce_loss = (-onehot_targets*logits.softmax(1).clamp(1e-7, 1.0).log()).sum(1)
+        if reduction == 'mean':
+            rce_loss = rce_loss.mean()
+        elif reduction == 'sum':
+            rce_loss = rce_loss.sum()
+        return self.alpha * ce_loss + self.beta * rce_loss
